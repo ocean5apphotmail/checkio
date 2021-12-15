d9b95029dc4f93fb575955b30a3de78ecbce970a
ocean5apphotmail <ocean5app@hotmail.com>
2021-12-16 00:07:42 +0800

    backword each word

--- Home/Backward Each Word.py
+++ Home/Backward Each Word.py
@@ -14,9 +14,44 @@
 #  Precondition: The line consists only from alphabetical symbols and spaces.
 # How to improve this mission? https://github.com/CheckiO-Missions/checkio-mission-backward-each-word { 5 }
 
+# Great! But how does join() manage to insert needed spase quantity? The question is about the case
+# str.split(' ') will treat group of whitespaces as empty strings separated by whitespaces.
+# 'hello   world'.split(' ') == ['hello', '', '', 'world']
+def backward_string_by_word1(text):
+    return ' '.join(word[::-1] for word in text.split(' '))
+
 def backward_string_by_word(text: str) -> str:
     # your code here
-    return None
+    spaces = []
+
+    rwords = []
+    ss = ''
+    rtext = ''
+
+    if ' ' not in text:
+        return text[::-1]
+
+    words = text.split()
+
+    for s in text:
+        if s.isspace() == True :
+            ss = ss + s
+        else:
+            if len(ss) != 0:
+                spaces.append(ss)
+                ss = ''
+
+    for w in words:
+        rw = w[::-1]
+        rwords.append(rw)
+
+    for i in range(0,len(rwords)):
+        rtext = rtext + rwords[i]
+        if i != len(rwords)-1:
+            rtext = rtext + spaces[i]
+
+    return rtext
+
 
 
 if __name__ == '__main__':
